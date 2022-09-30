58ea801048977baf341d0b5ce7b279882e3d5f8e
ByeongJunAn <abj5237@nate.com>
2022-09-30 16:15:05 +0900

    2022-09-30 java14.ver

---
+++ src/chap6/Switch1Deom.java
@@ -0,0 +1,25 @@
+package chap6;
+
+public class Switch1Deom {
+
+	public static void main(String[] args) {
+		int number = 3;
+
+		switch (number) {
+		case 3:
+			System.out.println("*");
+
+			break;
+		case 2:
+			System.out.println("*");
+		case 1:
+			System.out.println("*");
+
+		default:
+			System.out.println("&");
+
+		}
+
+	}
+
+}
